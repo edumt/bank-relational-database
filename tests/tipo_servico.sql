1d9e2732665d1f71aad5bfef9b5b82df019efa2b
edumt <edumoura1997@gmail.com>
2021-09-02 21:01:35 -0300

    generated some mock data for testing

---
+++ tests/tipo_servico.sql
@@ -0,0 +1,15 @@
+INSERT IGNORE INTO tipo_servico VALUES (0,'interdum blandit nonummy');
+INSERT IGNORE INTO tipo_servico VALUES (1,'elementum justo ridiculus');
+INSERT IGNORE INTO tipo_servico VALUES (2,'quis eros natoque');
+INSERT IGNORE INTO tipo_servico VALUES (3,'rutrum volutpat aptent');
+INSERT IGNORE INTO tipo_servico VALUES (4,'Donec penatibus placerat');
+INSERT IGNORE INTO tipo_servico VALUES (5,'ante torquent at');
+INSERT IGNORE INTO tipo_servico VALUES (6,'tristique tortor dapibus');
+INSERT IGNORE INTO tipo_servico VALUES (7,'nunc enim habitant');
+INSERT IGNORE INTO tipo_servico VALUES (8,'rutrum placerat massa');
+INSERT IGNORE INTO tipo_servico VALUES (9,'Lorem senectus sem');
+INSERT IGNORE INTO tipo_servico VALUES (10,'pede urna sem');
+INSERT IGNORE INTO tipo_servico VALUES (11,'tincidunt nisl ad');
+INSERT IGNORE INTO tipo_servico VALUES (12,'augue torquent risus');
+INSERT IGNORE INTO tipo_servico VALUES (13,'ultrices ridiculus libero');
+INSERT IGNORE INTO tipo_servico VALUES (14,'ante ac consectetuer');
